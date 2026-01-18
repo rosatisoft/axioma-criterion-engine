290a9d142b1274cd751bd987af2689fd2f8a067b
rosatisoft <rosatisoft@gmail.com>
2026-01-18 00:06:22 -0800

    Create SoftContradictionList.py

---
+++ axioma_criterion_engine/v4_1/SoftContradictionList.py
@@ -0,0 +1,12 @@
+SOFT_CONTRADICTION_DEFAULT_ACTION = {
+    "normative_vs_evidence": "reframe",
+    "urgency_mismatch": "ask_followup",
+    "goal_vs_costs": "ask_followup",
+    "preservation_mismatch": "ask_followup",
+    "time_horizon_mismatch": "ask_followup",
+    "alternatives_ignored": "ask_followup",
+    "causal_attribution_drift": "ask_followup",
+    "semantic_ambiguity": "stop_and_refine",
+    "value_conflict": "ask_followup",
+    "agency_externalization": "lower_confidence",
+}
